b342e3f0abbb7c6b92f4d05231c3284523295582
Amrit Sinha <56241123+SinhaAmrit@users.noreply.github.com>
2022-12-19 22:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000;
-
-const app = express();
-
-//Enable body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-//Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/openai', require('./routes/openaiRoutes'));
-
-app.listen(port, () => console.log('listening on port ${port}'));
+const path=require("path"),express=require("express"),dotenv=require("dotenv").config(),port=process.env.PORT||5e3,app=express();app.use(express.json()),app.use(express.urlencoded({extended:!1})),app.use(express.static(path.join(__dirname,"public"))),app.use("/api",require("./routes/openaiRoutes")),app.listen(port,()=>console.log("listening on port ${port}"));
